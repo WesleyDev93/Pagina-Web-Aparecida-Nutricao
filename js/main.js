0b95fa9c9630fa7b64261d438faeda0c9617ad60
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-12 16:10:26 -0300

    arrumando erros

--- js/main.js
+++ js/main.js
@@ -4,49 +4,44 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll ('.paciente');
 
-for (var i = 0; i < pacientes.length; i++) {
-   
-    var paciente = pacientes[i];
+var pacientes = document.querySelectorAll(".paciente");
 
-    var tdPeso = paciente.querySelector ('.info-peso');
+for(var i = 0; i < pacientes.length ; i++){
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+
+    var tdImc = paciente.querySelector(".info-imc");
 
     var peso = tdPeso.textContent;
-    
-    var tdAltura = document.querySelector ('.info-altura');
-    
     var altura = tdAltura.textContent;
-    
-    var tdImc = paciente.querySelector ('.info-imc');
-    
-    var pesoEvalido = true;
-    var alturaEvalida = true;
-    
-    
-    if (alturaEvalida && pesoEvalido){
-    var imc = peso / (altura* altura);
-   
+
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if(peso <= 0 || peso >= 1000){
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido!";
+        pesoEhValido = false;
     }
-    
-    tdImc.textContent = imc.toFixed(2);
-    
-    
-    if (peso < 0 || peso > 1000){
-        console.log (" peso invalido !");
-        pesoEvalido = false;
-        tdImc.textContent = "Peso Invalido";
-        paciente.style.color = "red";
-        paciente.classList.add('paciente-invalido');
+
+    if(altura <= 0 || altura >= 3.0){
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
     }
+
+        
+
+        if(pesoEhValido && alturaEhValida){
+            var imc = peso / ( altura * altura);    
+           
+            tdImc.textContent = imc.toFixed(2);
+        }
+    }        
+
     
-    if (altura < 0 || altura > 3.00){
-        console.log (" altura invalida !");
-        alturaEvalido = false;
-        tdImc.textContent = " Altura Invalida";
-        paciente.style.color = "red";
-       
-    }
     
     
-    
-}
+
 
