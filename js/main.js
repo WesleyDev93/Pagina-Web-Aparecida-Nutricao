4929ed989c79e57afcf6770d6af9d53c2f5f4064
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-12 15:45:40 -0300

    usando variavel i no for

--- js/main.js
+++ js/main.js
@@ -2,40 +2,47 @@ var titulo = document.querySelector ('.titulo');
 titulo.textContent = "Aparecida Nutricionista";
 
 
-var paciente = document.querySelectorAll ('.paciente');
+var pacientes = document.querySelectorAll ('.paciente');
 
-var tdPeso = paciente.querySelector ('.info-peso');
+for (var i = 0; i < pacientes.length; i++) {
+   
+    var paciente = pacientes[i];
 
-var peso = tdPeso.textContent;
+    var tdPeso = paciente.querySelector ('.info-peso');
 
-var tdAltura = document.querySelector ('.info-altura');
-
-var altura = tdAltura.textContent;
-
-var tdImc = paciente.querySelector ('.info-imc');
-
-var pesoEvalido = true;
-var alturaEvalida = true;
-
-
-if (alturaEvalida && pesoEvalido){
-var imc = peso / (altura* altura);
-console.log (imc);
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = document.querySelector ('.info-altura');
+    
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector ('.info-imc');
+    
+    var pesoEvalido = true;
+    var alturaEvalida = true;
+    
+    
+    if (alturaEvalida && pesoEvalido){
+    var imc = peso / (altura* altura);
+    console.log (imc);
+    }
+    
+    tdImc.textContent = imc.toFixed(2);
+    
+    
+    if (peso < 0 || peso > 1000){
+        console.log (" peso invalido !");
+        pesoEvalido = false;
+        tdImc.textContent = "Peso Invalido";
+    }
+    
+    if (altura < 0 || altura > 3.00){
+        console.log (" altura invalida !");
+        alturaEvalido = false;
+        tdImc.textContent = " Altura Invalida";
+    }
+    
+    
+    
 }
-
-tdImc.textContent = imc;
-
-
-if (peso < 0 || peso > 1000){
-    console.log (" peso invalido !");
-    pesoEvalido = false;
-    tdImc.textContent = "Peso Invalido";
-}
-
-if (altura < 0 || altura > 3.00){
-    console.log (" altura invalida !");
-    alturaEvalido = false;
-    tdImc.textContent = " Altura Invalida";
-}
-
 
