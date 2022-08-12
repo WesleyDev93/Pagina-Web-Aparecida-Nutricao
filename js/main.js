89458ecc0a6baa5755526fb2420e1dda32c56777
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-12 14:30:22 -0300

    usando if

--- js/main.js
+++ js/main.js
@@ -5,3 +5,24 @@ titulo.textContent = "Aparecida Nutricionista";
 var paciente = document.querySelector ('#primeiropaciente');
 
 var tdPeso = paciente.querySelector ('.info-peso');
+
+var peso = tdPeso.textContent;
+
+var tdAltura = document.querySelector ('.info-altura');
+
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector ('.info-imc');
+
+
+
+var imc = peso / (altura* altura);
+console.log (imc);
+
+
+tdImc.textContent = imc;
+
+
+if (peso < 0 || peso > 1000){
+    console.log (" peso invalido !");
+}
