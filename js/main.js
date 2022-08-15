7d306d07340b6c6a0e8aa40a8679c5f1a9e6b295
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-15 12:28:52 -0300

    usando appendChild

--- js/main.js
+++ js/main.js
@@ -55,7 +55,25 @@ for(var i = 0; i < pacientes.length ; i++){
                 console.log ('fui clicado')
 
                 var pacienteTr = document.createElement ('tr');
+
+                var nomeTd = document.createElement ('td');
+                var pesoTd = document.createElement ('td');
+                var alturaTd = document.createElement ('td');
+                var gorduraTd = document.createElement ('td');
+                var imcTd = document.createElement ('td');
+
+
+                nomeTd.textContent = nome;
+                pesoTd.textContent = peso;
+                alturaTd.textContent = altura;
+                gorduraTd.textContent = gordura;
                 
+                pacienteTr.appendChild (nomeTd);
+                pacienteTr.appendChild (pesoTd);
+                pacienteTr.appendChild (alturaTd);
+                pacienteTr.appendChild (gorduraTd);
+                
+
 
     });
     
