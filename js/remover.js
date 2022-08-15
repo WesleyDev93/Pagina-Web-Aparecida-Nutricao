405b6176802af5a20f7705de75afb70f1de63a2a
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-15 16:07:27 -0300

    adicionando animação ao apagar

---
+++ js/remover.js
@@ -0,0 +1,16 @@
+ var pacientes = document.querySelectorAll ('.paciente');
+ 
+ var tabela = document.querySelector ("table");
+
+ pacientes.forEach(function(paciente){
+           paciente.addEventListener("dblclick", function(){
+            event.target.parentNode.classList.add("fadeOut");
+
+                  
+
+                  setTimeout (function(){
+                   event.target.parentNode.remove();
+                  },500);
+                  
+
+ });
