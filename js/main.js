92fb8b3e2fb8157ef4df39caf5b1a511de4aa6a1
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-15 12:46:58 -0300

    boas praticas de JS

--- js/main.js
+++ js/main.js
@@ -40,42 +40,5 @@ for(var i = 0; i < pacientes.length ; i++){
         }
     }        
 
-    var botaoAdicionar = document.querySelector ('#adicionar-paciente');
-
-    botaoAdicionar.addEventListener ('click', function(event){
-                event.preventDefault();
-                var form = document.querySelector ('#form-adiciona');
-
-                var nome = form.nome.value;
-                var peso = form.peso.value;
-                var altura = form.altura.value;
-                var gordura = form.gordura.value;
-
-                console.log (nome);
-                console.log ('fui clicado')
-
-                var pacienteTr = document.createElement ('tr');
-
-                var nomeTd = document.createElement ('td');
-                var pesoTd = document.createElement ('td');
-                var alturaTd = document.createElement ('td');
-                var gorduraTd = document.createElement ('td');
-                var imcTd = document.createElement ('td');
-
-
-                nomeTd.textContent = nome;
-                pesoTd.textContent = peso;
-                alturaTd.textContent = altura;
-                gorduraTd.textContent = gordura;
-                
-                pacienteTr.appendChild (nomeTd);
-                pacienteTr.appendChild (pesoTd);
-                pacienteTr.appendChild (alturaTd);
-                pacienteTr.appendChild (gorduraTd);
-                
-
-
-    });
-    
     
 
