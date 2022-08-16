653aca7c38fb0f5f00ecf1dc2a87afd8a6e88498
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-16 14:34:27 -0300

    adicionando classe para a busca

--- js/filtrar.js
+++ js/filtrar.js
@@ -2,4 +2,32 @@ var campoFiltro = document.querySelector ('#filtrar-tabela');
 
 console.log (campoFiltro);
 
+campoFiltro.addEventListener ('input', function(){
+     var paciente = document.querySelectorAll ('.paciente');
 
+    if (this.value.length > 0 ){
+           
+        for (let i = 0; i < paciente.length; i++) {
+            var paciente = pacientes [i];
+            var tdnome = paciente.querySelector ('.info-nome');
+             var nome = paciente.textContent;
+             var expressao = new RegExp (this.value, "i");
+             if (! expressao.test(nome)){
+                 paciente.classList.add('.invisivel');
+     
+             }else{
+                 paciente.classList.remove('invisivel');
+             }
+          }
+
+
+    }else{
+        for (let i = 0; i < paciente.length; i++){
+            var paciente = pacientes[i];
+            paciente.classList.remove ('invisivel')
+        }
+    }
+
+
+     
+})
